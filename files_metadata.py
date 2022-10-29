57e3424d208705eba056dcfba5e1ee7a19ef90c4
tvpeter <withtvpeter@gmail.com>
2022-10-29 20:05:53 +0100

    update files metadata script

--- files_metadata.py
+++ files_metadata.py
@@ -1,20 +1,23 @@
+from fileinput import filename
 from math import fabs
 import os
 import glob
 import re
 
 # ALL THE FILES IN THE DIRECTORY
-count = not_index_no = 0
+count = 0
 for root_dir, cur_dir, files in os.walk(r'./'):
-    count += len(files)
+    for file in files:
+        count += 1
 print('Total files:', count)
 
 
 # number of files that are not named index.md
 counter = 0
-for filename in glob.glob("**/*", recursive=True):
-    if not filename.endswith("index.md"):
-        counter += 1
+for root_dir, cur_dir, files in os.walk(r'./'):
+    for file in files:
+        if not file.endswith("index.md"):
+            counter += 1
 print("Files that are not index.md: ", counter)
 
 
@@ -22,10 +25,19 @@ print("Files that are not index.md: ", counter)
 # files that don't end in .es or .pt (the english ones)
 
 not_es_or_pt_files = 0
-for filename in glob.glob("**/*", recursive=True):
-    if not filename.endswith(".es.md") and not filename.endswith(".pt") and not filename.endswith(".es"):
-        not_es_or_pt_files += 1
+for root_dir, cur_dir, files in os.walk(r'./'):
+    for file in files:
+        if not file.endswith(".es.md") and not file.endswith(".pt") and not file.endswith(".es"):
+            not_es_or_pt_files += 1
 print("Files that does not end with pt or es: ", not_es_or_pt_files)
+
+
+not_es_pt_or_index = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    for file in files:
+        if not file.endswith(".es.md") and not file.endswith(".pt") and not file.endswith(".es") and not file.endswith("index.md"):
+            not_es_pt_or_index += 1
+print("Files that are not index, pt or es: ", not_es_pt_or_index)
 
 
 def search_str(file_path, word):
@@ -36,62 +48,89 @@ def search_str(file_path, word):
     return False
         
 
-# files without the metadata have a speaker that can be parsed from the file name
+#  files without the metadata have a speaker that can be parsed from the file name
 #not index or .es
 # does not have name
 no_author_files = 0
 for root_dir, cur_dir, files in os.walk(r'./'):
     for name in files:
         if not name.endswith("index.md") and not name.endswith(".es.md"):
-            file_name = os.path.abspath(os.path.join(root_dir, name))
-            if not search_str(file_name, "name") and not search_str(file_name, "author"):
+            file_path = os.path.abspath(os.path.join(root_dir, name))
+            if not search_str(file_path, "name"):
                 no_author_files += 1
 
 print("Files without author name: ", no_author_files)
 
+with_author_name = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    for name in files:
+        if not name.endswith("index.md") and not name.endswith(".es.md"):
+            file_path = os.path.abspath(os.path.join(root_dir, name))
+            if search_str(file_path, "name"):
+                with_author_name += 1
 
-#files without the metadata have a date that can be parsed from the file name
-# will need to come back to this
+print("Files with author name: ", with_author_name)
 
-#how many have a video link 
+
+# how many have a video link 
 video_links = 0
 for root_dir, cur_dir, files in os.walk(r'./'):
     for name in files:
         if not name.endswith("index.md") and not name.endswith(".es.md"):
             file_name = os.path.abspath(os.path.join(root_dir, name))
-            if not search_str(file_name, "video"):
+            if not search_str(file_name, "video:"):
                 video_links += 1
 
 print("Files with video links: ", video_links)
 
 
-def find_date(date_file):
+def find_date(date_file, pattern):
     f = open(date_file, encoding="utf8", errors='ignore')
 
     content = f.read()
 
-    # date pattern
-    pattern = "\d{2}[/-]\d{2}[/-]\d{4}"
-
     dates = re.findall(pattern, content)
-
-    for date in dates:
+    if len(dates) > 0:
         return True
-
-    return False
+    else:
+        return False
 
 
 # Files with a date
-files_with_date = 0
+files_with_date = no_with_year = 0
+pattern = "\d{4}[/-]\d{2}[/-]\d{2}"
+year_last_pattern = "\d{2}[/-]\d{2}[/-]\d{4}"
+year_pattern = "\d{4}"
+
+for root_dir, cur_dir, files in os.walk(r'./'):
+    for name in files:
+        if not name.endswith("index.md") or not name.endswith(".es.md"):
+            file_name = os.path.abspath(os.path.join(root_dir, name))
+            if find_date(file_name, year_pattern):
+                no_with_year += 1
+            if find_date(file_name, pattern) or find_date(file_name, year_last_pattern):
+                files_with_date += 1
+
+print("Files with year date: yyyy ", no_with_year)
+print("Files with a date. Format yyyy-mm-mm or dd-mm-yyyy: ", files_with_date)
+
+
+
+# Files without a date
+without_date = filename_with_year = only_year = 0
 for root_dir, cur_dir, files in os.walk(r'./'):
     for name in files:
         if not name.endswith("index.md") and not name.endswith(".es.md"):
             file_name = os.path.abspath(os.path.join(root_dir, name))
-            if not find_date(file_name):
-                files_with_date += 1
+            if not find_date(file_name, pattern) and not find_date(file_name, year_last_pattern):
+                without_date += 1
+                if len(re.findall(pattern, file_name)) > 0:
+                    filename_with_year += 1
+                if len(re.findall(year_pattern, file_name)) > 0:
+                    only_year += 1
 
-print("Files with a date: ", files_with_date)
-
-
+print("Files without a date. Format yyyy-mm-mm or dd-mm-yyyy: ", without_date)
+print("Files without a date in body but has date [yyyy-mm-dd] in filename ", filename_with_year)
+print("Files without a date in body but has year [yyyy] in filename ", only_year)
 
 
