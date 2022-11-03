5e1644fbe9b890d1249bee1da9f8c441b4c6d790
tvpeter <withtvpeter@gmail.com>
2022-11-03 04:41:30 +0100

    add tags and categories fn

--- files_metadata.py
+++ files_metadata.py
@@ -5,7 +5,7 @@ import glob
 import re
 
 
-# Total Files: 984
+# Total Files: 978
 # Files that are not index.md: 873
 # Files do not end with pt or es: 907
 # Files that are not index, pt or es: 796
@@ -20,109 +20,112 @@ import re
 
 
 # ALL THE FILES IN THE DIRECTORY
-# count = 0
-# for root_dir, cur_dir, files in os.walk(r'./'):
-#     cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
-#     for file in files:
-#         count += 1
-# print('Total files:', count)
+count = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for file in files:
+        count += 1
+print('Total files:', count)
 
 
 # number of files that are not named index.md
-# counter = 0
-# for root_dir, cur_dir, files in os.walk(r'./'):
-#     cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
-#     for file in files:
-#         if not file.endswith("index.md"):
-#             counter += 1
-# print("Files that are not index.md: ", counter)
+counter = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for file in files:
+        if not file.endswith("index.md"):
+            counter += 1
+print("Files that are not index.md: ", counter)
 
 
 
-# # files that don't end in .es or .pt (the english ones)
+# files that don't end in .es or .pt (the english ones)
 
-# not_es_or_pt_files = 0
-# for root_dir, cur_dir, files in os.walk(r'./'):
-#     cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
-#     for file in files:
-#         if not file.endswith(".es.md") and not file.endswith(".pt") and not file.endswith(".es"):
-#             not_es_or_pt_files += 1
-# print("Files that does not end with pt or es: ", not_es_or_pt_files)
+not_es_or_pt_files = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for file in files:
+        if not file.endswith(".es.md") and not file.endswith(".pt") and not file.endswith(".es"):
+            not_es_or_pt_files += 1
+print("Files that does not end with pt or es: ", not_es_or_pt_files)
 
 
-# not_es_pt_or_index = 0
-# for root_dir, cur_dir, files in os.walk(r'./'):
-#     cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
-#     for file in files:
-#         if not file.endswith(".es.md") and not file.endswith(".pt") and not file.endswith(".es") and not file.endswith("index.md"):
-#             not_es_pt_or_index += 1
-# print("Files that are not index, pt or es: ", not_es_pt_or_index)
+not_es_pt_or_index = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for file in files:
+        if not file.endswith(".es.md") and not file.endswith(".pt") and not file.endswith(".es") and not file.endswith("index.md"):
+            not_es_pt_or_index += 1
+print("Files that are not index, pt or es: ", not_es_pt_or_index)
 
 
 def search_str(file_path, word):
+   lines_counter = 0
    with open(file_path, encoding="utf8", errors='ignore') as file:
     for line_number, line in enumerate(file, start=1):  
+        lines_counter += 1
         if word in line:
           return True
+        if lines_counter == 17: break
     return False
         
 
  #files without the metadata have a speaker that can be parsed from the file name
 #not index or .es
 #does not have name
-# no_author_files = 0
-# for root_dir, cur_dir, files in os.walk(r'./'):
-#     cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
-#     for name in files:
-#         if not name.endswith("index.md") and not name.endswith(".es.md"):
-#             file_path = os.path.abspath(os.path.join(root_dir, name))
-#             if not search_str(file_path, "name") and not search_str(file_path, "Name") and not search_str(file_path, "NAME"):
-#                 no_author_files += 1
-#                 print(file_path)
-#                 # with open('files_without_author.txt', 'a') as fd:
-#                 #     fd.write(f'\n{file_path}')
+no_author_files = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for name in files:
+        if not name.endswith("index.md") and not name.endswith(".es.md"):
+            file_path = os.path.abspath(os.path.join(root_dir, name))
+            if not search_str(file_path, "name") and not search_str(file_path, "Name") and not search_str(file_path, "NAME"):
+                no_author_files += 1
+                with open('files_without_author.txt', 'a') as fd:
+                    fd.write(f'\n{file_path}')
 
-# print("Files without author name: ", no_author_files)
+print("Files without author name: ", no_author_files)
 
-# with_author_name = 0
-# for root_dir, cur_dir, files in os.walk(r'./'):
-#     cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
-#     for name in files:
-#         if not name.endswith("index.md") and not name.endswith(".es.md"):
-#             file_path = os.path.abspath(os.path.join(root_dir, name))
-#             if search_str(file_path, "name") or search_str(file_path, "Name") or search_str(file_path, "NAME"):
-#                 with_author_name += 1
+with_author_name = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for name in files:
+        if not name.endswith("index.md") and not name.endswith(".es.md"):
+            file_path = os.path.abspath(os.path.join(root_dir, name))
+            if search_str(file_path, "name") or search_str(file_path, "Name") or search_str(file_path, "NAME"):
+                with_author_name += 1
 
-# print("Files with author name: ", with_author_name)
+print("Files with author name: ", with_author_name)
 
 
-# # without video links
-# without_video_link = 0
-# for root_dir, cur_dir, files in os.walk(r'./'):
-#     cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
-#     for name in files:
-#         if not name.endswith("index.md") and not name.endswith(".es.md"):
-#             file_name = os.path.abspath(os.path.join(root_dir, name))
-#             if not search_str(file_name, "video:") and not search_str(file_name, "Video:") and not search_str(file_name, "youtube"):
-#                 without_video_link += 1
-#                 print(file_name)
-#                 with open('files_without_video_link.txt', 'a') as fd:
-#                     fd.write(f'\n{file_name}')
+# without video links
+without_video_link = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for name in files:
+        if not name.endswith("index.md") and not name.endswith(".es.md"):
+            file_name = os.path.abspath(os.path.join(root_dir, name))
+            if not search_str(file_name, "https://www.youtube.com/watch"):
+                without_video_link += 1
+                print(file_name)
+                # with open('files_without_video_link.txt', 'a') as fd:
+                #     fd.write(f'\n{file_name}')
 
-# print("Files without video links: ", without_video_link)
+print("Files without video links: ", without_video_link)
 
 
 # files with video link 
-# video_links = 0
-# for root_dir, cur_dir, files in os.walk(r'./'):
-#     cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
-#     for name in files:
-#         if not name.endswith("index.md") and not name.endswith(".es.md"):
-#             file_name = os.path.abspath(os.path.join(root_dir, name))
-#             if search_str(file_name, "video:"):
-#                 video_links += 1
+video_links = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for name in files:
+        if not name.endswith("index.md") and not name.endswith(".es.md"):
+            file_name = os.path.abspath(os.path.join(root_dir, name))
+            if search_str(file_name, "https://www.youtube.com/watch"):
+                video_links += 1
+                # print(file_name)
 
-# print("Files with video links: ", video_links)
+print("Files with video links: ", video_links)
 
 
 def find_date(date_file, pattern):
@@ -137,24 +140,24 @@ def find_date(date_file, pattern):
         return False
 
 
-# # Files with a date
+# Files with a date
 files_with_date = no_with_year = 0
 pattern = "\d{4}[/-]\d{2}[/-]\d{2}"
 year_last_pattern = "\d{2}[/-]\d{2}[/-]\d{4}"
 year_pattern = "\d{4}"
 
-# for root_dir, cur_dir, files in os.walk(r'./'):
-#     cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
-#     for name in files:
-#         if not name.endswith("index.md") or not name.endswith(".es.md"):
-#             file_name = os.path.abspath(os.path.join(root_dir, name))
-#             if find_date(file_name, year_pattern):
-#                 no_with_year += 1
-#             if find_date(file_name, pattern) or find_date(file_name, year_last_pattern):
-#                 files_with_date += 1
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for name in files:
+        if not name.endswith("index.md") or not name.endswith(".es.md"):
+            file_name = os.path.abspath(os.path.join(root_dir, name))
+            if find_date(file_name, year_pattern):
+                no_with_year += 1
+            if find_date(file_name, pattern) or find_date(file_name, year_last_pattern):
+                files_with_date += 1
 
-# print("Files with year date: yyyy ", no_with_year)
-# print("Files with a date. Format yyyy-mm-mm or dd-mm-yyyy: ", files_with_date)
+print("Files with year date: yyyy ", no_with_year)
+print("Files with a date. Format yyyy-mm-mm or dd-mm-yyyy: ", files_with_date)
 
 
 
@@ -167,8 +170,9 @@ for root_dir, cur_dir, files in os.walk(r'./'):
             file_name = os.path.abspath(os.path.join(root_dir, name))
             if not find_date(file_name, pattern) and not find_date(file_name, year_last_pattern):
                 without_date += 1
-                # with open('files_without_date.txt', 'a') as fd:
-                #   fd.write(f'\n{file_name}')
+                print(file_name)
+                with open('files_without_date.txt', 'a') as fd:
+                  fd.write(f'\n{file_name}')
 
                 if len(re.findall(pattern, file_name)) > 0:
                     filename_with_year += 1
@@ -178,5 +182,45 @@ for root_dir, cur_dir, files in os.walk(r'./'):
 print("Files without a date. Format yyyy-mm-mm or dd-mm-yyyy: ", without_date)
 print("Files without a date in body but has date [yyyy-mm-dd] in filename ", filename_with_year)
 print("Files without a date in body but has year [yyyy] in filename ", only_year)
+
+
+# tagged files
+category_tag = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for name in files:
+        if not name.endswith("index.md") and not name.endswith(".es.md"):
+            file_name = os.path.abspath(os.path.join(root_dir, name))
+            if search_str(file_name, "categories:"):
+                category_tag += 1
+                # print(file_name)
+
+print("Files that have a category tag: ", category_tag)
+
+
+tags = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for name in files:
+        if not name.endswith("index.md") and not name.endswith(".es.md"):
+            file_name = os.path.abspath(os.path.join(root_dir, name))
+            if search_str(file_name, "tags"):
+                tags += 1
+                # print(file_name)
+
+print("Files that have a tags: ", tags)
+
+ # both category and tags
+both_cat_and_tags = 0
+for root_dir, cur_dir, files in os.walk(r'./'):
+    cur_dir[:] = [d for d in cur_dir if not d.startswith('.')]
+    for name in files:
+        if not name.endswith("index.md") and not name.endswith(".es.md"):
+            file_name = os.path.abspath(os.path.join(root_dir, name))
+            if search_str(file_name, "tags") and search_str(file_name, "categories:"):
+                both_cat_and_tags += 1
+                # print(file_name)
+
+print("Files that have both a category and tags: ", both_cat_and_tags)
 
 
